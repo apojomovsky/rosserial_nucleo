11d4a26a956360acb090ceec1a05ea39ec3c1240
Alexis Pojomovsky <apojomovsky@gmail.com>
2019-06-02 18:49:18 -0300

    Added initial files

---
+++ Src/start.cpp
@@ -0,0 +1,17 @@
+/*
+ * start.cpp
+ *
+ *  Created on: May 30, 2019
+ *      Author: alexis
+ */
+
+#import <ros.h>
+
+extern "C" int start_cpp() {
+
+	for(int i=0; i<5; ++i)
+	{
+		continue;
+	}
+	return 0;
+}
